7a199d0eee8b655bc551d23a7dfd1b4e6fb66750
Iraqbaghdad1 <ahmed_mohammed2000@yahoo.com>
2024-01-31 21:11:02 +0500

    List view setup

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from streamlit_folium import folium_static
 from streamlit_gsheets import GSheetsConnection
 from streamlit_option_menu import option_menu
 
+from template.html import card_view, review_card
 from utils import pre_process_data, pre_process_reviews, create_map
 
 # ------------------------------ Page Configuration------------------------------
@@ -59,4 +60,49 @@ if menu == "Pharmacies Map":
     pharmacies_map = create_map(data)
     st_data = folium_static(pharmacies_map, width=1500, height=650)
 
+if menu == "List View":
+    filters = st.columns(3)
+    stars = filters[0].multiselect(label="Rating", options=[5, 4, 3, 2, 1], placeholder="All")
+    reviews = filters[1].multiselect(label="Min. Reviewers",
+                                     options=["Up-to 50", "50 to 100", "100-200", "More than 200"],
+                                     placeholder="All")
+    city = filters[2].multiselect(label="City", options=data["city"].unique(), placeholder="All")
 
+    if not stars:
+        stars = [5, 4, 3, 2, 1]
+    if not reviews:
+        reviews = data["adjustedReview"].unique()
+    if not city:
+        city = data["city"].unique()
+
+    df = data.copy()
+    df = df[df["adjustedRating"].isin(stars)]
+    df = df[df["adjustedReview"].isin(reviews)]
+    df = df[df["city"].isin(city)]
+
+    st.write("# ")
+
+    pharmacies = df.sort_values(by="averageRating", ascending=False)
+
+    for i, pharmacy in pharmacies.iterrows():
+        upper_row = st.columns(2)
+        pharmacy_reviews = reviews_data[reviews_data.place_Name == pharmacy["name"]]
+        with upper_row[0]:
+            row = st.columns((1, 5))
+            row[0].image(r"./assets/icon-min.png")
+            row[1].markdown(card_view(pharmacy["name"], pharmacy["address"],
+                                      f"{pharmacy['averageRating']:.1f}", pharmacy["totalReviews"],
+                                      pharmacy["contact"]),
+                            unsafe_allow_html=True)
+        with upper_row[1]:
+            review_bar = st.expander(label="Reviews")
+            with review_bar:
+                for _, review in pharmacy_reviews.iterrows():
+                    row_ = st.columns((1, 6))
+                    row_[0].image(r"./assets/reviewer.png")
+                    row_[1].markdown(review_card(review['reviewer'], review['date'],
+                                                 review['rating']),
+                                     unsafe_allow_html=True)
+                    st.write(f"{review['text']}")
+                    st.write("---")
+        st.write("---")
--- template/html.py
+++ template/html.py
@@ -1,3 +1,4 @@
+# ----------------------- CUSTOMIZED HTML COMPONENTS ------------------------------
 
 
 POPUP = """
@@ -30,3 +31,57 @@ POPUP = """
         </div>
     </div>
 """
+
+
+def card_view(name, address, rating, reviews, contact):
+    return f"""
+        <div>
+            <div>
+                <div style='font-size:1.5rem;'><b>{name}</b></div>
+                <div style='color:#e63946;'><b>{address}</b></div>
+            </div>
+            <br>
+            <div style="align-items:center;display: flex;">
+                <button style='background-color: #4a5759;border: 0;border-radius: 56px;
+                                color: #fff;display: inline-block;
+                                font-size: 18px; fontweight: 600; outline: 0; padding: 10px 21px; position: relative; 
+                                text-align: center; text-decoration: none; transition: all .3s; user-select: none; 
+                                -webkit-user-select: none; touch-action: manipulation;margin-right:15px;width:25%;'>
+                        {rating} ⭐
+                </button>
+                <button style='background-color: #4a5759;border: 0;border-radius: 56px;
+                                color: #fff;display: inline-block;
+                                font-size: 18px; fontweight: 600; outline: 0; padding: 10px 21px; position: relative; 
+                                text-align: center; text-decoration: none; transition: all .3s; user-select: none; 
+                                -webkit-user-select: none; touch-action: manipulation;margin-right:15px;width:25%;'>
+                        {int(reviews)} 👥
+                </button>
+                <a style='background-color: #4a5759;border: 0;border-radius: 56px;
+                                color: #fff;cursor: pointer;display: inline-block;
+                                font-size: 18px; fontweight: 600; outline: 0; padding: 10px 21px; position: relative; 
+                                text-align: center; text-decoration: none; transition: all .3s; user-select: none; 
+                                margin-right:15px;width:25%;'href='tel:{contact}'">
+                  Dial 🌐
+                </a>
+            </div>
+        </div>
+        """
+
+
+def review_card(name, date, stars):
+    return f"""
+        <div>
+            <div style="align-items:center;display: flex;justify-content: space-between;">
+                <div style="display: inline-block;">
+                    <div style='font-size:1.5rem;'><b>{name}</b></div>
+                    <div style='color:#415a77;'><b>{date}</b></div>
+                </div>
+                <button style='background-color: #4a5759;border: 0;border-radius: 12%;
+                                color: #fff;display: inline-block;
+                                font-size: 18px; fontweight: 600; outline: 0; padding: 10px 21px; position: relative; 
+                                text-align: center; text-decoration: none; user-select: none; width:15%;'>
+                        {stars} ⭐
+                </button>
+            </div>
+        </div>
+        """
