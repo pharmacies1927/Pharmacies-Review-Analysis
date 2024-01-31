db76cd769d521277f33717cf234d8674e0c6a9de
Iraqbaghdad1 <ahmed_mohammed2000@yahoo.com>
2024-01-31 22:44:50 +0500

    data conn fixed

--- app.py
+++ app.py
@@ -45,12 +45,8 @@ st.markdown("""
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Get the spreadsheet associated with the connection
-spreadsheet = conn.get_spreadsheet()
-
-# Read data from the desired worksheets
-data = spreadsheet.read_worksheet("Pharmacies")
-reviews_data = spreadsheet.read_worksheet("AllReviews")
+data = conn.read(worksheet="Pharmacies")
+reviews_data = conn.read(worksheet="AllReviews")
 
 # data = pd.read_json("./data/Pharmacies.json")
 # data = data.transpose()
