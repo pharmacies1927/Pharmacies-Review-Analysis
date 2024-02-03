03d6f8cfaa57cb825deabf3ec8ef57272530840d
noorulhudaajmal <noorulhudaajmal12@gmail.com>
2024-02-03 07:19:01 +0500

    'Market Analysis' tab updated

--- plots.py
+++ plots.py
@@ -236,8 +236,8 @@ def pharmacies_choropleth(df):
 def top_performing_places(df):
     df = df.dropna(subset=["averageRating"])
     df["rank"] = (df["averageRating"] / df["totalReviews"])*100
-    df.sort_values(by="rank", ascending=False, inplace=True)
-    top_places = df.head(25)
+    df.sort_values(by="rank", ascending=True, inplace=True)
+    top_places = df.head(30)
     fig = go.Figure(
         go.Bar(
             y=top_places["name"],
